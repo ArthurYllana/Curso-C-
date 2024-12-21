b77fab6dfd4576145d749e1da5db6e644c7df380
ArthurYllana <arthur.yllana@gmail.com>
2024-12-21 15:20:56 -0300

    Interpolação e concatenação

--- Contatenação/Program.cs
+++ Contatenação/Program.cs
@@ -5,3 +5,5 @@ string nome = "André";
 System.Console.WriteLine("{0} tem {1} anos e seu saldo é {2:C}", nome, idade, saldo);
 
 System.Console.WriteLine($"{nome} tem {idade} anos e seu saldo é de {saldo:C} reais");
+
+System.Console.WriteLine(nome + " tem " + idade + " anos e seu saldo é de " + saldo + " reais");

28ef932607c9d9d64cb7333dd396b175f24357bc
ArthurYllana <arthur.yllana@gmail.com>
2024-12-21 15:22:11 -0300

    Interpolação e concatenação

--- Contatenação/Program.cs
+++ Contatenação/Program.cs
@@ -2,8 +2,8 @@
 double saldo = 10.3256;
 string nome = "André";
 
-System.Console.WriteLine("{0} tem {1} anos e seu saldo é {2:C}", nome, idade, saldo);
+System.Console.WriteLine("{0} tem {1} anos e seu saldo é de {2:C} rais", nome, idade, saldo);
 
 System.Console.WriteLine($"{nome} tem {idade} anos e seu saldo é de {saldo:C} reais");
 
-System.Console.WriteLine(nome + " tem " + idade + " anos e seu saldo é de " + saldo + " reais");
+System.Console.WriteLine(nome + " tem " + idade + " anos e seu saldo é de R$" + saldo.ToString("F2") + " reais");
--- Contatenação/obj/Debug/net8.0/Contatenação.AssemblyInfo.cs
+++ Contatenação/obj/Debug/net8.0/Contatenação.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contatenação")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdc67ec6cf11b08a7fdac1ea99c856470aeb9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77fab6dfd4576145d749e1da5db6e644c7df380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contatenação")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contatenação")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

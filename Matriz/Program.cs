171674d3e1d08f5586b3b0630cab0ab70191e384
ArthurYllana <arthur.yllana@gmail.com>
2025-01-04 16:19:19 -0300

    Lista de exercícios

---
+++ Matriz/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n, n]; 
+
+for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+        string[] values =Console.ReadLine().Split(' ');
+        mat[i, j] = int.Parse(Console.ReadLine()){
+            mat[i, j] = int.Parse(values[j]);
+        }
+    }
+
+    System.Console.WriteLine("Main Diagonal:");
+    for (int i = 0; i < n; i++){
+        System.Console.Write(mat[i, i] + " ");
+    }
+
+    int count = 0;
+    System.Console.WriteLine();
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if (mat[i, j] < 0) {
+                count++;
+            }
+        }
+    }
+    System.Console.WriteLine("Negative numbers count: " + count);
+}
---
+++ Var/Program.cs
@@ -0,0 +1,8 @@
+﻿var x = 10;
+var y = 20.0;
+var z = "Maria";
+//var -> Uma comodidade, mas não usar com frequência, pois pode causar problemas de tipagem. Exemplo: var x = 10; var x
+
+System.Console.WriteLine(x);
+System.Console.WriteLine(y);
+System.Console.WriteLine(z);
